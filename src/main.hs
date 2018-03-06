690ede0b0a9d13bcaf818497fc3099d846324c35
chrokh <christopher.okhravi@gmail.com>
2018-03-06 15:37:08 +0100

    Allow expressing stochastic double point curves.

--- src/main.hs
+++ src/main.hs
@@ -282,6 +282,7 @@ data StochasticActivity = StochasticActivity { timeDist :: Distribution Int
 data StochasticCurve = StochasticIdCurve
                      | StochasticGoalCurve Shape (Distribution Double)
                      | StochasticAreaCurve Shape (Distribution Double)
+                     | StochasticPntsCurve Shape (Distribution Double) (Distribution Double)
 
 
 -- When passing a StochasticProject and a seed to the function sampleProject we
@@ -317,10 +318,15 @@ sampleCurve :: StochasticCurve -> Seed -> (Curve, Seed)
 sampleCurve StochasticIdCurve seed = (IdCurve, seed)
 sampleCurve (StochasticGoalCurve shape dist) seed =
   let x = sample dist seed
-   in (GoalCurve shape (fst x), (snd x))
+   in (GoalCurve shape (fst x), snd x)
 sampleCurve (StochasticAreaCurve shape dist) seed =
   let x = sample dist seed
-   in (AreaCurve shape (fst x), (snd x))
+   in (AreaCurve shape (fst x), snd x)
+sampleCurve (StochasticPntsCurve shape d1 d2) seed =
+  let x1 = sample d1 seed
+      x2 = sample d2 (snd x1)
+   in (PntsCurve shape (fst x1) (fst x2), snd x2)
+
 
 
 
