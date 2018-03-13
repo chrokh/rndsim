89e2c3b590b71ef4adc6d8693ef4bb58b000477b
chrokh <christopher.okhravi@gmail.com>
2018-03-13 11:24:31 +0100

    Add the Discoverable type and the two stochastic functions succeeds and samplicate that simplify the implementation of the discovery function.

--- src/main.hs
+++ src/main.hs
@@ -61,6 +61,28 @@ sampleAll (hd : tl) s = let this = sample hd s
                          in (smpl : fst next, snd next)
 
 
+-- Assuming that a probability is between 0 and 1 we can call succeeds with the
+-- probability and a seed, and get back the next seed and a bool that represent
+-- whether the probabilistic activity suceeded or not.
+
+type Prob = Double
+succeeds :: Prob -> Seed -> (Bool, Seed)
+succeeds p s = let x = sample (Uniform (0 :: Double) (1 :: Double)) s
+                in (fst x <= p, snd x)
+
+
+-- samplicate works like replicate but replicates the constant c + X many times
+-- where c is the integer base of the passed double, and X is a random variable
+-- where the probability of 1 is equal to the decimals of the passed double.
+
+samplicate :: a -> Double -> Seed -> ([a], Seed)
+samplicate x p s
+  | p > 1     = let next = samplicate x (p - 1) s
+                 in (x : fst next, snd next)
+  | otherwise = let roll = succeeds p s
+                    dots = if (fst roll) then [x] else []
+                 in (dots, snd roll)
+
 
 
 ----------------------------------------------
@@ -468,6 +490,21 @@ ex2 = [ ActivityDist { timeDist = Uniform 6 12
                      , probDist = IdCurveDist
                      }
       ]
+
+
+
+
+----------------------------------------------
+  -- Basic research / Discovery
+----------------------------------------------
+
+data Discoverable = Discoverable { freq :: Double
+                                 , proj :: ProjectDist
+                                 }
+
+instance Samplable Discoverable [Project] where
+  sample disc seed = let dists = samplicate (proj disc) (freq disc) seed
+                      in sampleAll (fst dists) (snd dists)
 
 
 
